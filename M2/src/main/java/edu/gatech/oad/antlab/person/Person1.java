47db58c9de273af15e4a0b0a711c6e43e24d9f86
Kate Berman <kberman9@gatech.edu>
2018-09-07 10:24:59 -0400

    commit

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -1,53 +1,55 @@
 package edu.gatech.oad.antlab.person;
+
 /**
- *  A simple class for person 1
+ *  A simple class for person 5
  *  returns their name and a
- *  modified string
- *
- *  @author Sranee Bayapureddy sbayapureddy
- *  @version 1.1
+ *  modified string 
+ *  
+ *  @author Kate
+ *  @version 2.0
  */
-public class Person1 {
+public class Person5 {
   /** Holds the persons real name */
   private String name;
-    /**
-     * The constructor, takes in the persons
-     * name
-     * @param pname the person's real name
-     */
-  public Person1(String pname) {
+  	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+  public Person5(String pname) {
     name = pname;
   }
-    /**
-     * This method should take the string
-     * input and return its characters rotated
-     * 2 positions.
-     * given "gtg123b" it should return
-     * "g123bgt".
-     *
-     * @param input the string to be modified
-     * @return the modified string
-     */
-    private static String calc(String input) {
-      String returnstring = "";
-      for (int i = 2; i < input.length(); i++) {
-        returnstring = returnstring + input.charAt(i);
-      }
-      returnstring = returnstring + input.charAt(0);
-      returnstring = returnstring + input.charAt(1);
-      return returnstring;
-    }
-
-    /**
-     * Return a string rep of this object
-     * that varies with an input string
-     *
-     * @param input the varying string
-     * @return the string representing the
-     *         object
-     */
-    public String toString(String input) {
-      return name + calc(input);
-    }
+  	/**
+	 * This method should take the string
+	 * input and return its characters rotated
+	 * 2 positions.
+	 * given "gtg123b" it should return
+	 * "g123bgt".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private static String calc(String input) {
+	  //Person 5 put your implementation here
+	  String output = new String("");
+	  for(int x = 2; x < input.length(); x++){
+	  	output = output + input.charAt(x);
+	  }
+	  output = output + input.charAt(0);
+	  output = output + input.charAt(1);
+	  return output;
+	}
+	
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) {
+	  return name + calc(input);
+	}
 
 }
