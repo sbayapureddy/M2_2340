0e7794fb97b70e9f98f86be691d24cc7f05655fa
Dohyung Kim <dkswhale@gmail.com>
2018-09-11 00:20:49 -0400

    AntlabMain changed

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,7 +66,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Doh");
-		  toPrint += p3.toString("gburdell3");
+		  toPrint += p3.toString("dkim446");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Greg");
